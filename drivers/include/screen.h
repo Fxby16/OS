86858ae5af5827623556785ac1532d92ed6b0dda
Fabio <fabioscp0602@gmail.com>
2024-07-09 16:25:33 +0200

    comments

--- drivers/include/screen.h
+++ drivers/include/screen.h
@@ -6,6 +6,8 @@
 #define MAX_COLS 80
 #define WHITE_ON_BLACK 0x0F
 
+#define CHARACTER_SIZE 2    // 2 bytes -> character + attribute
+
 // Screen device I/O ports
 #define REG_SCREEN_CTRL 0x3D4
 #define REG_SCREEN_DATA 0x3D5
--- drivers/screen.c
+++ drivers/screen.c
@@ -5,7 +5,7 @@
 int get_screen_offset(int row, int col);
 int get_cursor();
 void set_cursor(int offset);
-void memory_copy(char *source, char *dest, int no_bytes);
+void memory_copy(char *source, char *dest, int bytes_count);
 int handle_scrolling(int cursor_offset);
 
 void port_byte_out(unsigned short port, unsigned char data);
@@ -15,26 +15,26 @@ void print_char(char ch, int row, int col, char attribute_byte)
 {
     unsigned char* vidmem = (unsigned char*) VIDEO_ADDRESS;
     
-    if(!attribute_byte){
+    if(!attribute_byte){    // if attribute byte is zero, assume white on black
         attribute_byte = WHITE_ON_BLACK;
     }
 
     int offset;
-    if(row >= 0 && col >= 0){
+    if(row >= 0 && col >= 0){   // if row and col are provided, calculate offset
         offset = get_screen_offset(row, col);
-    } else{
+    } else{                     // otherwise, use the current cursor position
         offset = get_cursor();
     }
 
-    if(ch == '\n'){
-        int rows = offset / (2 * MAX_COLS);
-        offset = get_screen_offset(rows, MAX_COLS - 1);
+    if(ch == '\n'){                                         // handle newline character
+        int rows = offset / (CHARACTER_SIZE * MAX_COLS);    // calculate current row
+        offset = get_screen_offset(rows, MAX_COLS - 1);     // set offset to the end of the row
     }else{
-        vidmem[offset] = ch;
-        vidmem[offset + 1] = attribute_byte;
+        vidmem[offset] = ch;                                // write the character to the video memory
+        vidmem[offset + 1] = attribute_byte;                // write the attribute byte to the video memory
     }
 
-    offset += 2;
+    offset += CHARACTER_SIZE;
 
     offset = handle_scrolling(offset);
     set_cursor(offset);
@@ -42,29 +42,33 @@ void print_char(char ch, int row, int col, char attribute_byte)
 
 int get_screen_offset(int row, int col)
 {
-    return (row * MAX_COLS + col) * 2;
+    return (row * MAX_COLS + col) * CHARACTER_SIZE;
 }
 
 int get_cursor()
 {
     int offset;
 
+    // request high byte of cursor offset (data 14)
     port_byte_out(REG_SCREEN_CTRL, 14);
     offset = port_byte_in(REG_SCREEN_DATA) << 8;
 
+    // request low byte of cursor offset (data 15)
     port_byte_out(REG_SCREEN_CTRL, 15);
     offset += port_byte_in(REG_SCREEN_DATA);
     
-    return offset * 2;
+    return offset * CHARACTER_SIZE;
 }
 
 void set_cursor(int offset)
 {
-    offset /= 2;
+    offset /= CHARACTER_SIZE;
 
+    // write high byte of cursor offset (data 14)
     port_byte_out(REG_SCREEN_CTRL, 14);
     port_byte_out(REG_SCREEN_DATA, (unsigned char) (offset >> 8));
     
+    // write low byte of cursor offset (data 15)
     port_byte_out(REG_SCREEN_CTRL, 15);
     port_byte_out(REG_SCREEN_DATA, (unsigned char) (offset & 0xff));
 }
@@ -91,6 +95,7 @@ void clear_screen()
     int row = 0;
     int col = 0;
 
+    // simply fill the screen with spaces
     for(row = 0; row < MAX_ROWS; row++){
         for(col = 0; col < MAX_COLS; col++){
             print_char(' ', row, col, WHITE_ON_BLACK);
@@ -100,39 +105,50 @@ void clear_screen()
     set_cursor(get_screen_offset(0, 0));
 }
 
-void memory_copy(char* source, char* dest, int no_bytes)
+void memory_copy(char* source, char* dest, int bytes_count)
 {
     int i;
-    for(i = 0; i < no_bytes; i++){
+    for(i = 0; i < bytes_count; i++){
         *(dest + i) = *(source + i);
     }
 }
 
 int handle_scrolling(int cursor_offset)
 {
-    if(cursor_offset < MAX_ROWS * MAX_COLS * 2){
+    // if cursor offset is within the screen, return it
+    if(cursor_offset < MAX_ROWS * MAX_COLS * CHARACTER_SIZE){
         return cursor_offset;
     }
 
     int i;
+    
+    // move all rows one row up
     for(i = 1; i < MAX_ROWS; i++){
-        memory_copy((char*) (get_screen_offset(i, 0) + VIDEO_ADDRESS), (char*) (get_screen_offset(i - 1, 0) + VIDEO_ADDRESS), MAX_COLS * 2);
+        memory_copy((char*) (get_screen_offset(i, 0) + VIDEO_ADDRESS), 
+                    (char*) (get_screen_offset(i - 1, 0) + VIDEO_ADDRESS), 
+                    MAX_COLS * CHARACTER_SIZE);
     }
 
+    // clear the last row
     char *last_line = (char*) (get_screen_offset(MAX_ROWS - 1, 0) + VIDEO_ADDRESS);
-    for(i = 0; i < MAX_COLS * 2; i++){
+    for(i = 0; i < MAX_COLS * CHARACTER_SIZE; i++){
         last_line[i] = 0;
     }
 
-    cursor_offset -= 2 * MAX_COLS;
+    // move cursor one row up
+    cursor_offset -= CHARACTER_SIZE * MAX_COLS;
 
     return cursor_offset;
 }
 
+// I/O functions
+
+// write data to a port
 void port_byte_out(unsigned short port, unsigned char data) {
     asm volatile("outb %0, %1" : : "a"(data), "Nd"(port));
 }
 
+// read data from a port
 unsigned char port_byte_in(unsigned short port) {
     unsigned char result;
     asm volatile("inb %1, %0"
